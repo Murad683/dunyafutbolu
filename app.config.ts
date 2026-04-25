9a5d83c9e655173aa539343b7bb68960858912ff
Murad683 <muradmemedov16@gmail.com>
2026-04-25 23:05:51 +0500

    changes

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from '@tanstack/react-start/config'
 
+console.log("APP.CONFIG.TS IS LOADED! PRESET IS VERCEL");
+
 export default defineConfig({
   server: {
     preset: 'vercel'
