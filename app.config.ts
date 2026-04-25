2458d0f4b197df6879f721860b218757dd6b3872
Murad683 <muradmemedov16@gmail.com>
2026-04-25 23:24:44 +0500

    Remove Vercel traces

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from '@tanstack/react-start/config'
 
-console.log("APP.CONFIG.TS IS LOADED! PRESET IS VERCEL");
-
 export default defineConfig({
   server: {
     preset: 'netlify'
